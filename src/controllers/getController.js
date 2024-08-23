ecd79163f65d842908e9f3a86eb2e19f0fa7523e
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-23 19:02:07 -0300

    refactor: Update controllers to use repository modules

--- src/controllers/getController.js
+++ src/controllers/getController.js
@@ -1,58 +1,48 @@
-import { ObjectId } from "mongodb";
-import { db } from "../databases/mongodb.js";
-import { count } from "../middleware/counter.js";
+import { count } from '../middleware/counter.js';
+import choiceRepository from '../repositories/choiceRepository.js';
+import pollRepository from '../repositories/pollRepository.js';
 
 async function getPolls(req, res) {
-  try {
-    const allPolls = await db.collection("polls").find().toArray();
-    res.status(200).send(allPolls);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+	try {
+		const allPolls = await pollRepository.getPolls();
+		res.status(200).send(allPolls);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
 }
 
 async function getChoices(req, res) {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  try {
-    const choicesForThePoll = await db
-      .collection("choices")
-      .find({ poolId: id })
-      .toArray();
+	try {
+		const choicesForThePoll = await choiceRepository.getChoicesByPollId(id);
 
-    res.status(200).send(choicesForThePoll);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+		res.status(200).send(choicesForThePoll);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
 }
 
 async function getResult(req, res) {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  try {
-    const poll = await db.collection("polls").findOne(ObjectId(id));
+	try {
+		const poll = await pollRepository.getPollById(id);
 
-    if (!poll) return res.sendStatus(404);
+		if (!poll) return res.sendStatus(404);
 
-    await db.collection("polls").updateOne(
-      { _id: ObjectId(id) },
-      {
-        $set: {
-          result: {
-            title: poll.title,
-            votes: count,
-          },
-        },
-      }
-    );
+		await pollRepository.updatePoll(id, {
+			title: poll.title,
+			votes: count,
+		});
 
-    res.sendStatus(201);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+		res.sendStatus(201);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
 }
 
 export { getPolls, getChoices, getResult };
--- src/controllers/postControllers.js
+++ src/controllers/postControllers.js
@@ -1,91 +1,88 @@
-import { db } from "../databases/mongodb.js";
-import { ObjectId } from "mongodb";
-import dayjs from "dayjs";
+import dayjs from 'dayjs';
+
+import choiceRepository from '../repositories/choiceRepository.js';
+import pollRepository from '../repositories/pollRepository.js';
+import voteRepository from '../repositories/voteRepository.js';
 
 async function pollPost(req, res) {
-  const { title, expireAt } = req.body;
-  let date = expireAt;
+	const { title, expireAt } = req.body;
+	let date = expireAt;
 
-  if (title === "") return res.sendStatus(422);
+	if (title === '') return res.sendStatus(422);
 
-  if (expireAt === "") {
-    const defaultPoll = dayjs().add("30", "day");
-    date = defaultPoll.format("YYYY-MM-DD HH:mm");
-  }
+	if (expireAt === '') {
+		const defaultPoll = dayjs().add('30', 'day');
+		date = defaultPoll.format('YYYY-MM-DD HH:mm');
+	}
 
-  try {
-    const pollTitle = await db.collection("polls").findOne({ title: title });
+	try {
+		const pollTitle = await pollRepository.getPollByTitle(title);
 
-    if (pollTitle.title === title) return res.sendStatus(409);
+		if (pollTitle.title === title) return res.sendStatus(409);
 
-    await db.collection("polls").insertOne({ title, expireAt: date });
+		await pollRepository.createPoll(title, date);
 
-    res.sendStatus(201);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+		res.sendStatus(201);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
 }
 
 async function pollChoices(req, res) {
-  const { title, poolId } = req.body;
+	const { title, poolId } = req.body;
 
-  try {
-    const existingPoll = await db.collection("polls").findOne(ObjectId(poolId));
+	try {
+		const existingPoll = await pollRepository.getPollById(poolId);
 
-    if (!existingPoll) return res.sendStatus(404);
+		if (!existingPoll) return res.sendStatus(404);
 
-    if (title === "") return res.sendStatus(422);
+		if (title === '') return res.sendStatus(422);
 
-    const choice = await db.collection("choices").findOne({ title: title });
+		const choice = await choiceRepository.getChoicesByTitle(title);
 
-    if (choice.title === title) return res.sendStatus(409);
+		if (choice.title === title) return res.sendStatus(409);
 
-    const inicialDate = Date.parse(existingPoll.expireAt);
-    const currentDate = Date.parse(dayjs().format("YYYY-MM-DD HH:mm"));
-    const timeDifference = inicialDate - currentDate;
+		const inicialDate = Date.parse(existingPoll.expireAt);
+		const currentDate = Date.parse(dayjs().format('YYYY-MM-DD HH:mm'));
+		const timeDifference = inicialDate - currentDate;
 
-    if (timeDifference <= 0) return res.sendStatus(403);
+		if (timeDifference <= 0) return res.sendStatus(403);
 
-    await db
-      .collection("choices")
-      .insertOne({ title, poolId: ObjectId(poolId) });
-    res.sendStatus(201);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+		await choiceRepository.createChoice(title, poolId);
+		res.sendStatus(201);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
 }
 
 async function pollVote(req, res) {
-  const { id } = req.params;
-  const creationDate = dayjs().format("YYYY-MM-DD HH:mm");
+	const { id } = req.params;
+	const creationDate = dayjs().format('YYYY-MM-DD HH:mm');
 
-  try {
-    const vote = await db.collection("choices").findOne(ObjectId(id));
+	try {
+		const vote = await choiceRepository.getChoiceById(id);
 
-    if (!vote) res.sendStatus(404);
+		if (!vote) res.sendStatus(404);
 
-    const existingPoll = await db.collection("polls").findOne(ObjectId(poolId));
+		const existingPoll = await pollRepository.getPollById(vote.pollId);
 
-    if (!existingPoll) return res.sendStatus(404);
+		if (!existingPoll) return res.sendStatus(404);
 
-    const inicialDate = Date.parse(existingPoll.expireAt);
-    const currentDate = Date.parse(creationDate);
-    const timeDifference = inicialDate - currentDate;
+		const inicialDate = Date.parse(existingPoll.expireAt);
+		const currentDate = Date.parse(creationDate);
+		const timeDifference = inicialDate - currentDate;
 
-    if (timeDifference <= 0) return res.sendStatus(403);
+		if (timeDifference <= 0) return res.sendStatus(403);
 
-    await db.collection("vote").insertOne({
-      createdAt: creationDate,
-      choiceId: ObjectId(id),
-    });
+		await voteRepository.createVote(creationDate, id);
 
-    res.sendStatus(201);
-  } catch (error) {
-    console.error(error);
-    res.sendStatus(500);
-  }
+		res.sendStatus(201);
+	} catch (error) {
+		console.error(error);
+		res.sendStatus(500);
+	}
 }
 
 export { pollPost, pollChoices, pollVote };
