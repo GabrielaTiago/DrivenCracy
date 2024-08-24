fd229f2af63afde43f88b53a69a67971e626fb99
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-24 00:24:09 -0300

    feat: Add schemasValidation middleware to validate data in routes

--- src/routes/choicesRouter.js
+++ src/routes/choicesRouter.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import choicesController from '../controllers/choicesController.js';
 import { countVotes } from '../middleware/counter.js';
+import schemasValidation from '../middleware/schemasValidation.js';
 
 const { pollChoices, pollVote } = choicesController;
 
 const choicesRouter = Router();
 
-choicesRouter.post('/choice', pollChoices);
+choicesRouter.post('/choice', schemasValidation('choice'), pollChoices);
 choicesRouter.post('/choice/:id/vote', countVotes, pollVote);
 
 export default choicesRouter;
--- src/routes/pollsRouter.js
+++ src/routes/pollsRouter.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import poolsController from '../controllers/poolsController.js';
 import choicesController from '../controllers/choicesController.js';
+import schemasValidation from '../middleware/schemasValidation.js';
 
 const { createPoll, getPolls, getResult } = poolsController;
 const { getChoices } = choicesController;
@@ -8,7 +9,7 @@ const { getChoices } = choicesController;
 const poolsRouter = Router();
 
 poolsRouter.get('/poll', getPolls);
-poolsRouter.post('/poll', createPoll);
+poolsRouter.post('/poll', schemasValidation('pool'), createPoll);
 poolsRouter.get('/poll/:id/choice', getChoices);
 poolsRouter.get('/poll/:id/result', getResult);
 
