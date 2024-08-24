4fd031d51d87ac296a618fe7548be1847dc30624
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-23 23:05:33 -0300

    feat: Add schemas module for validating data

---
+++ src/schemas/schemas.js
@@ -0,0 +1,10 @@
+import choiceSchema from './choiceSchema';
+import poolSchema from './poolSchema';
+
+const schemas = {
+	choice: choiceSchema,
+	pool: poolSchema,
+	vote: voteSchema,
+};
+
+export default schemas;
