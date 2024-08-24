b9bb7903d8cfac6f0663eb6d9f56835b382479ef
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-24 20:31:32 -0300

    feat: Add service to create a new choice into a poll

--- src/services/choicesService.js
+++ src/services/choicesService.js
@@ -1,5 +1,7 @@
+import dayjs from 'dayjs';
 import customError from '../errors/errors.js';
 import choiceRepository from '../repositories/choiceRepository.js';
+import pollRepository from '../repositories/pollRepository.js';
 
 async function getAllChoicesFromPoll(pollId) {
 	const choices = await choiceRepository.getChoicesByPollId(pollId);
@@ -9,8 +11,29 @@ async function getAllChoicesFromPoll(pollId) {
 	return choices;
 }
 
+async function createNewChoice(title, pollId) {
+	const poll = await pollRepository.getPollById(pollId);
+
+	if (!poll) customError('not_found', 'Enquete não encontrada');
+
+	const pollChoices = await choiceRepository.getChoicesByPollId(pollId);
+
+	for (const choice of pollChoices) {
+		if (choice.title === title) customError('conflict', 'Opção já cadastrada nessa enquete');
+	}
+
+	const pollExpirationDate = poll.expireAt;
+
+	const isAnActivePoll = dayjs().isBefore(dayjs(pollExpirationDate));
+
+	if (!isAnActivePoll) customError('forbidden', 'Enquete expirada');
+
+	await choiceRepository.createChoice(title, pollId);
+}
+
 const choicesService = {
 	getAllChoicesFromPoll,
+	createNewChoice,
 };
 
 export default choicesService;
