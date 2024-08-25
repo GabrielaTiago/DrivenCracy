ecd2b807cdd0f516ba2d148d37639ab3a3e83a64
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-24 21:18:06 -0300

    refactor: Update choicesController to use choicesService for voting on a poll choice

--- src/controllers/choicesController.js
+++ src/controllers/choicesController.js
@@ -23,30 +23,10 @@ async function pollChoices(req, res) {
 
 async function pollVote(req, res) {
 	const { id: choiceId } = req.params;
-	const now = dayjs().format('YYYY-MM-DD HH:mm');
 
-	try {
-		const pollId = await choiceRepository.getPollIdByChoiceId(choiceId);
+	await choicesService.voteOnThePollChoice(choiceId);
 
-		if (!pollId) res.sendStatus(404);
-
-		const existingPoll = await pollRepository.getPollById(pollId);
-
-		if (!existingPoll) return res.sendStatus(404);
-
-		const endDate = Date.parse(existingPoll.expireAt);
-		const currentDate = Date.parse(now);
-		const timeDifference = endDate - currentDate;
-
-		if (timeDifference <= 0) return res.sendStatus(403);
-
-		await voteRepository.createVote(now, choiceId);
-
-		res.sendStatus(201);
-	} catch (error) {
-		console.error(error);
-		res.sendStatus(500);
-	}
+	res.sendStatus(201);
 }
 
 const choicesController = {
