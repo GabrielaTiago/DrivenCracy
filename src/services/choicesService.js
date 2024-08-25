636826d44b5d0405dd494727e0662145d717ce30
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2024-08-24 21:17:54 -0300

    feat: Add  service function to vote on the poll choice

--- src/services/choicesService.js
+++ src/services/choicesService.js
@@ -2,6 +2,7 @@ import dayjs from 'dayjs';
 import customError from '../errors/errors.js';
 import choiceRepository from '../repositories/choiceRepository.js';
 import pollRepository from '../repositories/pollRepository.js';
+import voteRepository from '../repositories/voteRepository.js';
 
 async function getAllChoicesFromPoll(pollId) {
 	const choices = await choiceRepository.getChoicesByPollId(pollId);
@@ -31,9 +32,29 @@ async function createNewChoice(title, pollId) {
 	await choiceRepository.createChoice(title, pollId);
 }
 
+async function voteOnThePollChoice(choiceId) {
+	const now = dayjs().format('YYYY-MM-DD HH:mm');
+	const pollId = await choiceRepository.getPollIdByChoiceId(choiceId);
+
+	if (!pollId) customError('not_found', 'Nenhuma enquete encontrada para esta opção');
+
+	const poll = await pollRepository.getPollById(pollId);
+
+	if (!poll) customError('not_found', 'Enquete não encontrada');
+
+	const pollExpirationDate = poll.expireAt;
+
+	const isAnActivePoll = dayjs().isBefore(dayjs(pollExpirationDate));
+
+	if (!isAnActivePoll) customError('forbidden', 'Enquete expirada');
+
+	await voteRepository.createVote(now, choiceId);
+}
+
 const choicesService = {
 	getAllChoicesFromPoll,
 	createNewChoice,
+	voteOnThePollChoice,
 };
 
 export default choicesService;
